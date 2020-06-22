215800016aafe685b77fb97f926d18c4dd9d49dd
Snehal Seth <sethsnehal95@gmail.com>
2020-06-22 14:38:37 +0530

    adds types for request options

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,9 +1,32 @@
+interface RequestOptions {
+  data: Record<string | number, any>;
+  params: Record<string | number, any>;
+}
+
+const buildQueryParams = (params: any) =>
+  Object.keys(params)
+    .map((k) => encodeURIComponent(k) + '=' + encodeURIComponent(params[k]))
+    .join('&');
+
 // TODO: use generic here to make API response type-safe
-export const request = async (method: string, url: string, options?: any) =>
-  await fetch(url, {
+export const request = async (
+  method: string,
+  url: string,
+  options?: RequestOptions,
+) => {
+  let apiUrl = url;
+
+  if (options?.params) {
+    apiUrl +=
+      (url.indexOf('?') === -1 ? '?' : '&') + buildQueryParams(options?.params);
+  }
+
+  return await fetch(apiUrl, {
     method,
     headers: {
       'Access-Control-Allow-Origin': '*',
     },
-    ...options,
+    // TODO: check this when using POST request
+    ...(options?.data && { body: JSON.stringify(options?.data) }),
   }).then((res) => res.json());
+};
