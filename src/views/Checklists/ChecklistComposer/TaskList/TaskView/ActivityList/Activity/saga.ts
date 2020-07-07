4ecfa3394ac60d1ae5b01df86647a11b031a9e97
Snehal Seth <sethsnehal95@gmail.com>
2020-07-07 13:19:09 +0530

    activity saga fix for single activity update action

--- src/views/Checklists/ChecklistComposer/TaskList/TaskView/ActivityList/Activity/saga.ts
+++ src/views/Checklists/ChecklistComposer/TaskList/TaskView/ActivityList/Activity/saga.ts
@@ -1,19 +1,12 @@
 import { takeLatest } from 'redux-saga/effects';
 
-import { updateActivity, updateActivityData } from './actions';
+import { updateActivity } from './actions';
 import { ActivityAction } from './types';
 
 function* updateActivitySaga({ payload }: ReturnType<typeof updateActivity>) {
   console.log('payload from updateActivitySaga :: ', payload);
 }
 
-function* updateActivityDataSaga({
-  payload,
-}: ReturnType<typeof updateActivityData>) {
-  console.log('payload from updateActivityDataSaga :: ', payload);
-}
-
 export function* ActivitySaga() {
-  yield takeLatest(ActivityAction.UPDATE_ACTIVITY_DATA, updateActivityDataSaga);
   yield takeLatest(ActivityAction.UPDATE_ACTIVITY, updateActivitySaga);
 }
