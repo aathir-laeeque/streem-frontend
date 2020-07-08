4c7a7a820d20fed8dd009dda49dfdcd3e79ddd20
Snehal Seth <sethsnehal95@gmail.com>
2020-07-08 19:30:02 +0530

    minor fix and build command change

--- src/views/Checklists/ChecklistComposer/Header/index.tsx
+++ src/views/Checklists/ChecklistComposer/Header/index.tsx
@@ -1,15 +1,10 @@
 import { Button } from '#components';
 import { useTypedSelector } from '#store';
-import { ChecklistState } from '#views/Checklists/types';
 import React, { FC } from 'react';
-import { useDispatch } from 'react-redux';
 
-import { setChecklistState } from '../actions';
 import { Wrapper } from './styles';
 
 const Header: FC = () => {
-  const dispatch = useDispatch();
-
   const { isChecklistEditable } = useTypedSelector(
     (state) => state.checklist.composer,
   );
@@ -18,15 +13,7 @@ const Header: FC = () => {
     <Wrapper>
       <div className="header-item">Stages</div>
       <span className="auto-save-text">All changes saved automatically</span>
-      <Button
-        onClick={() => {
-          if (isChecklistEditable) {
-            dispatch(setChecklistState(ChecklistState.EXECUTING));
-          } else {
-            dispatch(setChecklistState(ChecklistState.ADD_EDIT));
-          }
-        }}
-      >
+      <Button>
         {isChecklistEditable ? 'Publish Checklist' : 'Complete Job'}
       </Button>
     </Wrapper>
