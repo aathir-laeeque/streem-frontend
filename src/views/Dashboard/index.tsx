1c1facd431e55f8095481c016c4e265eb24b8352
Ashish Choudhary <ashish.c@leucinetech.com>
2020-08-04 17:38:44 +0530

    Added Under Construction Component

--- src/views/Dashboard/index.tsx
+++ src/views/Dashboard/index.tsx
@@ -1,9 +1,14 @@
 import React, { FC } from 'react';
 
+import MemoUnderConstruction from '#assets/svg/UnderConstruction';
 import { DashboardProps } from './types';
 
 const Dashboard: FC<DashboardProps> = () => {
-  return <div>Dashboard view</div>;
+  return (
+    <div>
+      <MemoUnderConstruction />
+    </div>
+  );
 };
 
 export default Dashboard;
