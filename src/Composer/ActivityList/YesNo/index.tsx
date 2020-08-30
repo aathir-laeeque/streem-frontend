162c401341eb1c167df23855d6141a3cd92d4c73
Snehal Seth <sethsnehal95@gmail.com>
2020-08-31 02:24:12 +0530

    uncomment refreshtoken poll and styles seperation

--- src/Composer/ActivityList/YesNo/index.tsx
+++ src/Composer/ActivityList/YesNo/index.tsx
@@ -1,84 +1,9 @@
 import { Entity } from '#Composer/types';
 import { useTypedSelector } from '#store';
 import React, { FC } from 'react';
-import styled from 'styled-components';
 
 import { ActivityProps } from '../types';
-
-const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-
-  .buttons-container {
-    display: flex;
-    margin-top: 12px;
-
-    .button-item {
-      /* flex: 1;
-
-      &.editing {
-        flex: 0;
-      } */
-
-      button {
-        background-color: transparent;
-        cursor: pointer;
-        font-size: 14px;
-        letter-spacing: 0.16px;
-        line-height: 1.29;
-        padding: 5px 20px;
-      }
-
-      :first-child {
-        input {
-          background-color: #e1fec0;
-          border-bottom-color: #27ae60;
-
-          :active,
-          :focus {
-            border-color: #27ae60;
-          }
-        }
-
-        button {
-          border: 1px solid #5aa700;
-          border-radius: 4px;
-          color: #5aa700;
-
-          &.filled {
-            color: #ffffff;
-            background-color: #427a00;
-          }
-        }
-      }
-
-      :last-child {
-        margin-left: 20px;
-
-        input {
-          background-color: #ffebeb;
-          border-bottom-color: #eb5757;
-
-          :active,
-          :focus {
-            border-color: #eb5757;
-          }
-        }
-
-        button {
-          border: 1px solid #ff6b6b;
-          border-radius: 4px;
-          color: #ff6b6b;
-
-          &.filled {
-            color: #ffffff;
-            background-color: #cc5656;
-          }
-        }
-      }
-    }
-  }
-`;
+import { Wrapper } from './styles';
 
 const YesNoActivity: FC<ActivityProps> = ({ activity }) => {
   const { entity } = useTypedSelector((state) => state.composer);
@@ -91,11 +16,13 @@ const YesNoActivity: FC<ActivityProps> = ({ activity }) => {
           <div className="buttons-container">
             {activity.data
               .sort((a, b) => (a.type > b.type ? -1 : 1))
-              .map((el, index) => (
-                <div key={index} className="button-item">
-                  <button>{el.name}</button>
-                </div>
-              ))}
+              .map((el, index) => {
+                return (
+                  <div key={index} className="button-item">
+                    <button>{el.name}</button>
+                  </div>
+                );
+              })}
           </div>
         </>
       ) : null}
