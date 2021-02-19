fc45563452435a2fa9c4805e25b68c4473beb2b5
Ashish Choudhary <ashish.c@leucinetech.com>
2021-02-19 10:35:48 +0530

    CU-257vr0 - An archived User should get logged out when they are archived

--- src/utils/axiosClient.ts
+++ src/utils/axiosClient.ts
@@ -54,14 +54,7 @@ axiosInstance.interceptors.response.use(
         const {
           auth: { isLoggedIn },
         } = store.getState();
-        if (
-          isLoggedIn &&
-          [
-            LoginErrorCodes.REFRESH_TOKEN_EXPIRED,
-            LoginErrorCodes.BLOCKED,
-            LoginErrorCodes.JWT_TOKEN_INVALID,
-          ].includes(code)
-        ) {
+        if (isLoggedIn && code !== LoginErrorCodes.INCORRECT) {
           throw message || 'Oops! Please Try Again.';
         } else {
           return response?.data;
