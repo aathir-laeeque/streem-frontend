6702dd07422b85fff4cf51b18ae8e096a6ecf752
Ashish Choudhary <ashish.c@leucinetech.com>
2021-02-26 10:43:38 +0530

    Change Disabled Message For Requested Changes State

--- src/PrototypeComposer/modals/EditingDisabled.tsx
+++ src/PrototypeComposer/modals/EditingDisabled.tsx
@@ -61,19 +61,16 @@ const EditingDisabledModal: FC<CommonOverlayProps<{
             } else {
               switch (state) {
                 case DisabledStates.SUBMITTED_FOR_REVIEW:
-                  return 'No edits can be done as the prototype is being reviewed.';
                 case DisabledStates.BEING_REVIEWED:
                   return 'No edits can be done as the prototype is being reviewed.';
                 case DisabledStates.READY_FOR_SIGNING:
-                  return 'No edits can be done as the prototype is under signing now';
                 case DisabledStates.SIGN_OFF_INITIATED:
-                  return 'No edits can be done as the prototype is under signing now';
                 case DisabledStates.SIGNING_IN_PROGRESS:
                   return 'No edits can be done as the prototype is under signing now';
                 case DisabledStates.READY_FOR_RELEASE:
                   return 'No edits can be done as the prototype is now ready for release';
                 case EnabledStates.REQUESTED_CHANGES:
-                  return 'No edits can be done as the prototype is being Modified.';
+                  return 'No edits can be done as the Prototype is with the Author for changes.';
                 case DisabledStates.PUBLISHED:
                   return 'You can start a revision of this Checklist to make changes';
                 default:
