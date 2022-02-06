645a1ced2bb6e538dda854dc475aa4c058cb0943
Mokshesh Dafariya <mokshesh@leucinetech.com>
2022-02-06 13:15:11 +0530

    bump: release from 2.0.0 to 2.0.1

--- scripts/post-build.sh
+++ scripts/post-build.sh
@@ -1,15 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
-# extract version from package.json
+# extract version & customer_version from package.json
+CUSTOMER_VERSION=$(sed -nE 's/^\s*"customer_version": "(.*?)",$/\1/p' package.json)
 VERSION=$(sed -nE 's/^\s*"version": "(.*?)",$/\1/p' package.json)
 
 # write version details to the file
-printf "Product: CLEEN\n\nVersion: $VERSION" > dist/version.txt
+printf "Product: CLEEN\n\nVersion: %s" "$CUSTOMER_VERSION" > dist/version.txt
 
 # fetch the git commit
 LAST_COMMIT_SHA=$(git rev-parse HEAD)
 BRANCH=$(git branch --show-current)
 
 # write git details to the file
-JSON='{"version":"%s","branch":"%s","commit":"%s"}\n'
-printf "$JSON" "$VERSION" "$BRANCH" "$LAST_COMMIT_SHA" > dist/version.json
+printf "{\"version\":\"%s\",\"branch\":\"%s\",\"commit\":\"%s\"}" "$VERSION" "$BRANCH" "$LAST_COMMIT_SHA" > dist/version.json
