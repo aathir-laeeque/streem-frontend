3787f80e6e45d1f85d7b818e4a8854e3e35a4188
RuchaLeucine <rucha.patil@leucinetech.com>
2023-07-03 12:04:28 +0530

    Add search bar on ontology page

--- src/views/Ontology/ObjectTypes/ObjectTypeList.tsx
+++ src/views/Ontology/ObjectTypes/ObjectTypeList.tsx
@@ -1,20 +1,31 @@
-import { Button, DataTable, LoadingContainer, Pagination, TabContentProps } from '#components';
+import {
+  Button,
+  DataTable,
+  LoadingContainer,
+  Pagination,
+  TabContentProps,
+  TextInput,
+} from '#components';
 import { useTypedSelector } from '#store';
 import { DEFAULT_PAGE_NUMBER, DEFAULT_PAGE_SIZE } from '#utils/constants';
 import { fetchDataParams } from '#utils/globalTypes';
 import { TabContentWrapper } from '#views/Jobs/ListView/styles';
 import { navigate } from '@reach/router';
-import React, { FC, useEffect } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { fetchObjectTypes } from '../actions';
 import checkPermission from '#services/uiPermissions';
 import { DataTableColumn } from '#components/shared/DataTable';
+import { Search } from '@material-ui/icons';
+import { debounce } from 'lodash';
 
 const ObjectTypeList: FC<TabContentProps> = ({ label, values }) => {
   const dispatch = useDispatch();
   const {
     objectTypes: { list, listLoading, pageable },
   } = useTypedSelector((state) => state.ontology);
+
+  const [searchQuery, setSearchQuery] = useState<string>('');
 
   const fetchData = (params: fetchDataParams = {}) => {
     const { page = DEFAULT_PAGE_NUMBER, size = DEFAULT_PAGE_SIZE } = params;
@@ -23,18 +34,28 @@ const ObjectTypeList: FC<TabContentProps> = ({ label, values }) => {
         page,
         size,
         usageStatus: 1,
+        displayName: searchQuery ? searchQuery : undefined,
       }),
     );
   };
 
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [searchQuery]);
 
   return (
     <TabContentWrapper>
-      {checkPermission(['ontology', 'createObjectType']) && (
-        <div className="filters">
+      <div className="filters">
+        <div style={{ maxWidth: '306px', width: '306px' }}>
+          <TextInput
+            afterElementWithoutError
+            AfterElement={Search}
+            afterElementClass=""
+            placeholder={`Search with Object Type`}
+            onChange={debounce(({ value }) => setSearchQuery(value), 500)}
+          />
+        </div>
+        {checkPermission(['ontology', 'createObjectType']) && (
           <Button
             id="create"
             onClick={() => {
@@ -43,9 +64,8 @@ const ObjectTypeList: FC<TabContentProps> = ({ label, values }) => {
           >
             Add New Object Type
           </Button>
-        </div>
-      )}
-
+        )}
+      </div>
       <LoadingContainer
         loading={listLoading}
         component={
