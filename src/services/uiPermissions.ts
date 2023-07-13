be9f14d361f5ab245c75cec8608c0b78ddf28879
RuchaLeucine <rucha.patil@leucinetech.com>
2023-07-13 19:48:24 +0530

    CU-85ztgcje7 - facility admin should not be able to revise and archive the checklist

--- src/services/uiPermissions.ts
+++ src/services/uiPermissions.ts
@@ -113,8 +113,8 @@ const uiPermissions: Record<string, any> = {
   checklists: {
     create: [roles.ACCOUNT_OWNER, roles.FACILITY_ADMIN, roles.CHECKLIST_PUBLISHER],
     createGlobal: [roles.ACCOUNT_OWNER, roles.GLOBAL_ADMIN],
-    revision: [roles.ACCOUNT_OWNER, roles.FACILITY_ADMIN, roles.CHECKLIST_PUBLISHER],
-    archive: [roles.ACCOUNT_OWNER, roles.FACILITY_ADMIN, roles.CHECKLIST_PUBLISHER],
+    revision: [roles.ACCOUNT_OWNER, roles.CHECKLIST_PUBLISHER],
+    archive: [roles.ACCOUNT_OWNER, roles.CHECKLIST_PUBLISHER],
     release: [roles.ACCOUNT_OWNER, roles.CHECKLIST_PUBLISHER],
     releaseGlobal: [roles.ACCOUNT_OWNER, roles.GLOBAL_ADMIN],
     createJob: [
