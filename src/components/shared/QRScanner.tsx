12bc72e617eb7d489f6efdade76a3e6c4c640b03
Ashish Choudhary <ashish.c@leucinetech.com>
2023-09-21 10:15:16 +0530

    CU-85ztz4qug - fix: after qr scan media track keeps playing
    Signed-off-by: Ashish Choudhary <ashish.c@leucinetech.com>

--- src/components/shared/QRScanner.tsx
+++ src/components/shared/QRScanner.tsx
@@ -1,54 +1,8 @@
 import { BaseModal } from '#components';
 import { CommonOverlayProps } from '#components/OverlayContainer/types';
-import { getVideoDevices } from '#utils/inputUtils';
-import React, { FC, useEffect } from 'react';
-import { QrReader } from 'react-qr-reader';
+import React, { FC, useEffect, useRef } from 'react';
+import QrScanner from 'qr-scanner';
 import styled from 'styled-components';
-
-const ViewFinderWrapper = styled.div`
-  .label {
-    top: calc(50dvh - 200px);
-    left: calc(50dvw - 6ch);
-    z-index: 2;
-    position: absolute;
-    color: rgb(255, 255, 255);
-    font-size: 20px;
-    font-weight: 600;
-  }
-
-  .overlay {
-    top: 0px;
-    left: 0px;
-    z-index: 1;
-    box-sizing: border-box;
-    border-inline: calc(50dvw - 300px) solid rgba(0, 0, 0, 0.72);
-    border-block: calc(50vh - 300px) solid rgba(0, 0, 0, 0.72);
-    position: absolute;
-    width: 100%;
-    height: 100%;
-  }
-
-  .quiet-zone {
-    top: -10%;
-    left: -10%;
-    z-index: 1;
-    box-sizing: border-box;
-    position: absolute;
-    width: 120%;
-    height: 120%;
-    border: rgb(29, 132, 255) solid 4px;
-    border-radius: 15px;
-  }
-`;
-
-const ViewFinder = () => (
-  <ViewFinderWrapper>
-    <span className="label">Scan QR Code</span>
-    <div className="overlay">
-      <div className="quiet-zone" />
-    </div>
-  </ViewFinderWrapper>
-);
 
 const Wrapper = styled.div`
   #modal-container {
@@ -64,10 +18,35 @@ const Wrapper = styled.div`
       }
 
       &-body {
-        > section {
-          > div {
-            padding-top: unset !important;
-            position: unset !important;
+        padding: 0 !important;
+        .video-container {
+          min-height: 100dvh;
+          min-width: 100dvw;
+          line-height: 0;
+          position: relative;
+          overflow: hidden;
+
+          .scan-region-highlight {
+            outline: rgba(0, 0, 0, 0.5) solid 50vmax;
+            .scan-region-highlight-svg {
+              stroke: #64a2f3 !important;
+              stroke-width: 8 !important;
+              width: 126% !important;
+              height: 126% !important;
+              left: -13% !important;
+              top: -13% !important;
+            }
+            .code-outline-highlight {
+              stroke: rgba(255, 255, 255, 0.8) !important;
+              stroke-width: 15 !important;
+              stroke-dasharray: none !important;
+            }
+          }
+
+          video {
+            height: 100dvh;
+            width: 100%;
+            object-fit: cover;
           }
         }
       }
@@ -85,9 +64,45 @@ export const QRScanner: FC<CommonOverlayProps<Props>> = ({
   closeOverlay,
   props: { onSuccess },
 }) => {
+  const scanner = useRef<QrScanner | null>(null);
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+  const foundResult = useRef(false);
+
+  const setResult = (result: any) => {
+    if (!foundResult.current) {
+      foundResult.current = true;
+      setTimeout(() => {
+        onSuccess(result);
+        closeOverlay();
+      }, 700);
+    }
+  };
+
   useEffect(() => {
+    if (!scanner.current && videoRef.current) {
+      scanner.current = new QrScanner(videoRef.current, setResult, {
+        highlightScanRegion: true,
+        highlightCodeOutline: true,
+        calculateScanRegion: (v) => {
+          const smallestDimension = Math.min(v.videoWidth, v.videoHeight);
+
+          // Make scan region smaller to match better small qr codes
+          const scanRegionSize = Math.round((1 / 4) * smallestDimension);
+
+          let region: QrScanner.ScanRegion = {
+            x: Math.round((v.videoWidth - scanRegionSize) / 2),
+            y: Math.round((v.videoHeight - scanRegionSize) / 2),
+            width: scanRegionSize,
+            height: scanRegionSize,
+          };
+          return region;
+        },
+      });
+      scanner.current.start();
+    }
     return () => {
-      getVideoDevices();
+      scanner.current?.stop();
+      scanner.current?.destroy();
     };
   }, []);
 
@@ -99,21 +114,9 @@ export const QRScanner: FC<CommonOverlayProps<Props>> = ({
         showHeader={false}
         showFooter={false}
       >
-        <QrReader
-          constraints={{ facingMode: { ideal: 'environment' } }}
-          videoId="video"
-          scanDelay={500}
-          ViewFinder={ViewFinder}
-          onResult={(result, error) => {
-            if (result) {
-              onSuccess(result?.getText());
-              closeOverlay();
-            }
-            if (error && error.message) {
-              console.log(error.message);
-            }
-          }}
-        />
+        <div className="video-container">
+          <video id="qr-video" ref={videoRef}></video>
+        </div>
       </BaseModal>
     </Wrapper>
   );
--- src/utils/inputUtils.ts
+++ src/utils/inputUtils.ts
@@ -1,12 +1,5 @@
 export const getVideoDevices = async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({ video: true });
   const allDevices = await navigator.mediaDevices.enumerateDevices();
   const videoDevices = allDevices.filter(({ kind }) => kind === 'videoinput');
-  const tracks = stream.getTracks();
-
-  for (let i = 0; i < tracks.length; i++) {
-    let track = tracks[i];
-    track.stop();
-  }
   return videoDevices || [];
 };
